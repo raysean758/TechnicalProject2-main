7acd66ba326bf3791a0973ae91c261ecf81190e4
raysean758 <rasonson@salcc.edu.lc>
2025-05-12 17:15:13 -0400

    dd

--- TechnicalProject2-main/inventory-software/app/(back-office)/dashboard/inventory/units/new/page.jsx
+++ TechnicalProject2-main/inventory-software/app/(back-office)/dashboard/inventory/units/new/page.jsx
@@ -1,69 +1,71 @@
-"use client"
-import FormHeader from '@/components/dashboard/FormHeader';
-import SubmitButton from '@/components/FormInputs/SubmitButton';
-import TextareaInput from '@/components/FormInputs/TextAreaInput';
-import TextInput from '@/components/FormInputs/TextInput';
+"use client";
+import FormHeader from "@/components/dashboard/FormHeader";
+import SubmitButton from "@/components/FormInputs/SubmitButton";
+import TextareaInput from "@/components/FormInputs/TextAreaInput";
+import TextInput from "@/components/FormInputs/TextInput";
 // Removed unused imports from 'lucide-react'
-import Link from 'next/link';
-import React, { useState } from 'react' ;
-import { useFormState } from 'react-dom';
-import { useForm, useFormContext } from 'react-hook-form';
+import Link from "next/link";
+import React, { useState } from "react";
+import { useFormState } from "react-dom";
+import { useForm, useFormContext } from "react-hook-form";
 
 export default function NewUnit() {
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm();
-    const [loading, setLoading] = useState(false)
-    async function onSubmit(data) {
-        console.log(data)
-        setLoading(true)
-        const baseUrl ="http://localhost:3000"
-        try {
-            const response =await fetch(`${baseUrl}/api/units"`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body:JSON.stringify(data)
-        });
-        if(response.ok){
-            console.log(response)
-           setLoading(false)
-            reset();
-        }
-        } catch (error) {
-            setLoading(false)
-            console.log(error)
-        }
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const [loading, setLoading] = useState(false);
+  async function onSubmit(data) {
+    console.log(data);
+    setLoading(true);
+    const baseUrl = "http://localhost:3000";
+    try {
+      const response = await fetch(`${baseUrl}/api/units"`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        console.log(response);
+        setLoading(false);
+        reset();
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
     }
-    return (
-        <div>
-            {/* Header */}
-            <FormHeader title="New Unit" href="/dashboard/inventory/"></FormHeader>
-            {/* Form */}
-            <form onSubmit={handleSubmit(onSubmit)} className='w-full max-w-3xl p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3'>
-                 <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">             
-                       <TextInput
-                        Label="Unit Title"
-                        name="title"
-                        register={register} 
-                        errors={errors}
-                        className='w-full'
-                />
-                 <TextInput
-                        Label="Unit Abbreviation"
-                        name="abbreviation"
-                        register={register} 
-                        errors={errors}
-                        className='w-full'
-                />
-              </div>
-                <SubmitButton isLoading={loading} 
-                title="Unit"/>
-            </form>
+  }
+  return (
+    <div>
+      {/* Header */}
+      <FormHeader title="New Unit" href="/dashboard/inventory/"></FormHeader>
+      {/* Form */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-3xl p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3"
+      >
+        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+          <TextInput
+            Label="Unit Title"
+            name="title"
+            register={register}
+            errors={errors}
+            className="w-full"
+          />
+          <TextInput
+            Label="Unit Abbreviation"
+            name="abbreviation"
+            register={register}
+            errors={errors}
+            className="w-full"
+          />
         </div>
-    );
+        <SubmitButton isLoading={loading} title="Unit" />
+      </form>
+    </div>
+  );
 }
