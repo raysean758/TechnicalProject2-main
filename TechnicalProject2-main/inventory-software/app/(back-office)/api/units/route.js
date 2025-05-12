db2be1bc76e5cb9969dcdff8874f2d0fd52e3c21
raysean758 <rasonson@salcc.edu.lc>
2025-05-12 16:40:53 -0400

    finally fixed

---
+++ TechnicalProject2-main/inventory-software/app/(back-office)/api/units/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { title, abbreviation } = await request.json();
+
+    const unit = { title, abbreviation };
+    console.log(unit);
+    return NextResponse.json(unit);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        error,
+        message: "Failed to Create a Unit"
+      },
+      {
+        status: 500
+      }
+    );
+  }
+}
---
+++ TechnicalProject2-main/inventory-software/app/(back-office)/dashboard/inventory/units/new/page.jsx
@@ -0,0 +1,74 @@
+"use client"
+import FormHeader from '@/components/dashboard/FormHeader';
+import SubmitButton from '@/components/FormInputs/SubmitButton';
+import TextareaInput from '@/components/FormInputs/TextAreaInput';
+import TextInput from '@/components/FormInputs/TextInput';
+// Removed unused imports from 'lucide-react'
+import Link from 'next/link';
+import React, { useState } from 'react' ;
+import { useFormState } from 'react-dom';
+import { useForm, useFormContext } from 'react-hook-form';
+
+export default function NewUnit() {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm();
+    const [loading, setLoading] = useState(false)
+    async function onSubmit(data) {
+        console.log(data)
+        setLoading(true)
+        const baseUrl ="http://localhost:3000"
+        try {
+            const response =await fetch(`${baseUrl}/api/categories"`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(data)
+        });
+        if(response.ok){
+            console.log(response)
+           setLoading(false)
+            reset();
+        }
+        } catch (error) {
+            setLoading(false)
+            console.log(error)
+        }
+    }
+    return (
+        <div>
+            {/* Header */}
+            <FormHeader title="New Category" href="/dashboard/inventory/"></FormHeader>
+            {/* Form */}
+            <form onSubmit={handleSubmit(onSubmit)} className='w-full max-w-3xl p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3'>
+                 <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">             
+                       <TextInput
+                        Label="Category Title"
+                        name="title"
+                        register={register} 
+                        errors={errors}
+                />
+                <div className="sm:col-span-2">
+                    <label
+                    htmlFor="description"
+                    className='block text-sm font-medium leading-6 text-gray-900'
+                    >
+                        Course Description
+                    </label>
+                    <TextareaInput
+                        Label="Category Description"
+                        name="description"
+                        register={register} 
+                        errors={errors}/>
+                                </div>
+                                </div>
+                <SubmitButton isLoading={loading} 
+                title="Category"/>
+            </form>
+        </div>
+    );
+}
